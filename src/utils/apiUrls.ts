05c80ced8d25bebedf0421991dd95eb7c154af18
Daniel Wiederman <danielwiederman@Daniels-MacBook-Pro.local>
2022-01-02 11:06:18 +0200

    fix url secured

--- src/utils/apiUrls.ts
+++ src/utils/apiUrls.ts
@@ -2,13 +2,13 @@
 const apiKey = "SAxmf6VAm1C3A0DWr8PdcQutXY0lDGvV";
 
 export const autoCompleteUrl = (query: string) =>
-  `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`;
+  `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${query}`;
 
 export const currentForecastUrl = (locationKey: string) =>
-  `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
+  `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
 
 export const futureForecastUrl = (locationKey: string) =>
-  `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`;
+  `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apiKey}`;
 
 export const geoLocationSearchUrl = (lat: number, long: number) =>
-  `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat},${long}`;
+  `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${apiKey}&q=${lat},${long}`;
